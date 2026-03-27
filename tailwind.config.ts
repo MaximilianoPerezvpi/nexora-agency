7bb4b5a5680021ea45341ab77e2262ebe760bfc2
MaximilianoPerezvpi <maxip3498@gmail.com>
2026-03-27 19:11:24 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#050505", // Negro profundo
+        surface: "#111111", // Gris muy oscuro para tarjetas
+        neon: "#EFFF00", // Amarillo Neón
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
